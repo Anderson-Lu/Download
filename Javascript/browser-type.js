3ae3e79c45d58f94c481e64b0252cc3bdb861f1f
Anderson Lu <Forward2015@users.noreply.github.com>
2017-03-13 13:01:17 +0800

    Update browser-type.js

--- Javascript/browser-type.js
+++ Javascript/browser-type.js
@@ -11,3 +11,16 @@ var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -
 var isFF = userAgent.indexOf("Firefox") > -1 ; 
 var isSafari = userAgent.indexOf("Safari") > -1 && userAgent.indexOf("Chrome") < 1 ; 
 var isChrome = userAgent.indexOf("Chrome") > -1 ;
+
+//Extend for isIE
+var isIE = function(ver){
+    var b = document.createElement('b')
+    b.innerHTML = '<!--[if IE ' + ver + ']><i></i><![endif]-->'
+    return b.getElementsByTagName('i').length === 1
+}
+if(isIE(6)){
+    // IE 6
+}
+if(isIE(9)){
+    // IE 9
+}
