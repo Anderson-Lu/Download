e4f7c60fdb40ec75a6bb6d9e2eaacf1f4d9783ff
Anderson Lu <Forward2015@users.noreply.github.com>
2017-03-13 12:59:30 +0800

    Update and rename is-ie.js to browser-type.js

---
+++ Javascript/browser-type.js
@@ -0,0 +1,13 @@
+var isIE = function(){
+    var b = document.createElement('b')
+    b.innerHTML = '<!--[if IE]><i></i><![endif]-->'
+    return b.getElementsByTagName('i').length === 1
+}
+
+var userAgent = navigator.userAgent; 
+var isOpera = userAgent.indexOf("Opera") > -1; 
+var isMaxthon = userAgent.indexOf("Maxthon") > -1 ; 
+var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1 && !isOpera ; 
+var isFF = userAgent.indexOf("Firefox") > -1 ; 
+var isSafari = userAgent.indexOf("Safari") > -1 && userAgent.indexOf("Chrome") < 1 ; 
+var isChrome = userAgent.indexOf("Chrome") > -1 ;
--- Javascript/is-ie.js
+++
@@ -1,5 +0,0 @@
-var isIE = function(){
-    var b = document.createElement('b')
-    b.innerHTML = '<!--[if IE]><i></i><![endif]-->'
-    return b.getElementsByTagName('i').length === 1
-}
